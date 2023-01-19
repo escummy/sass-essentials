6b3c68b819fdb0f7e3a1fc6a3ffdb1aead6e83bc
Gonzalo Cugiani <escummy@gmail.com>
2023-01-19 22:40:32 +0100

    feat: add grid to show table of movies

---
+++ src/components/MovieTable/MovieTable.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+import Grid from "@mui/material/Grid";
+import { MovieCard } from "../MovieCard/MovieCard";
+
+export const MovieTable = ({ movies, type }) => {
+  return (
+    <>
+      <Grid sx={{ flexGrow: 1 }} container spacing={2} mt={10} mb={10}>
+        <Grid item xs={12} m={1}>
+          <Grid container justifyContent="center" spacing={2}>
+            {movies.map((movie) => (
+              <div key={movie.id}>
+                <Grid item xs={15} mb={4}>
+                  <MovieCard movie={movie} type={type} />
+                </Grid>
+              </div>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
