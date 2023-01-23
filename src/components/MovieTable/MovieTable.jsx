228ecea71bc1aa9982d11699633063b405b210a3
Gonzalo Cugiani <escummy@gmail.com>
2023-01-23 06:03:12 +0100

    Change margin right and left in the grid

--- src/components/MovieTable/MovieTable.jsx
+++ src/components/MovieTable/MovieTable.jsx
@@ -10,7 +10,7 @@ export const MovieTable = ({ movies, type }) => {
           <Grid container justifyContent="center" spacing={2}>
             {movies.map((movie) => (
               <div key={movie.id}>
-                <Grid item xs={15} mb={4}>
+                <Grid item xs={15} mb={4} mr={1} ml={1}>
                   <MovieCard movie={movie} type={type} />
                 </Grid>
               </div>
